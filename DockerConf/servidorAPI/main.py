0f1e93f41637beb509fc9dd5b341c77b6a0ef1a9
Jorrge <orck10@gmail.com>
2020-06-10 06:51:29 -0300

    fix main

--- DockerConf/servidorAPI/main.py
+++ DockerConf/servidorAPI/main.py
@@ -1,20 +1,6 @@
 from views.mensagemApp import *
 from flask import Flask
 from flask_restful import Api
-#from  configparser import ConfigParser
-
-
-'''config = ConfigParser()
-config["MONGO"] = { "host":"$host$",
-                    "port":"$port$",
-                    "username":"$username$",
-                    "password":"$password$",
-                    "authSource":"$authSource$",
-                    "db":"$db$",
-                    "collection":"$collection$"}
-with open('./mongo.conf', 'w') as configfile:
-    config.write(configfile)
-'''
 
 app = Flask(__name__)
 api = Api(app)
